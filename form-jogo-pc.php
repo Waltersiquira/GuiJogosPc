34e9559e934d28784f10b63b62ca7da7507b6db0
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-11 12:17:25 -0300

    Create form-jogo-pc.php

---
+++ form-jogo-pc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icons/favicon.ico" type="image/x-icon">
+    <title>GuiJogosPc</title>
+</head>
+<body>
+    <form action="enviar-form-jogo-pc.php" method="get">
+        digite o nome do seu jogo de pc <br>
+        <input type="text" name="nome"> <br>
+        digite a descrição do seu jogo de pc <br>
+        <input type="text" name="descricao"> <br>
+        digite o preço do seu jogo de pc <br>
+        <input type="text" name="preco"> <br>
+        digite a url da imagem do jogo de pc <br>
+        <input type="text" name="imagem"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
